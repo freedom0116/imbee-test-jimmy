ad8fcc0306410e536baa5dd23bb2ec1f218e0225
DESKTOP-Q61474S\Jimmy <jimmy29257415@gmail.com>
2022-04-22 08:36:23 +0800

    feat: add mock data

---
+++ src/mock/tags.js
@@ -0,0 +1,79 @@
+const tags = {
+  items: [
+    {
+      has_synonyms: true,
+      is_moderator_only: false,
+      is_required: false,
+      count: 2364259,
+      name: 'javascript',
+    },
+    {
+      has_synonyms: true,
+      is_moderator_only: false,
+      is_required: false,
+      count: 1933188,
+      name: 'python',
+    },
+    {
+      has_synonyms: true,
+      is_moderator_only: false,
+      is_required: false,
+      count: 1840599,
+      name: 'java',
+    },
+    {
+      has_synonyms: true,
+      is_moderator_only: false,
+      is_required: false,
+      count: 1533522,
+      name: 'c#',
+    },
+    {
+      has_synonyms: true,
+      is_moderator_only: false,
+      is_required: false,
+      count: 1434050,
+      name: 'php',
+    },
+    {
+      has_synonyms: true,
+      is_moderator_only: false,
+      is_required: false,
+      count: 1371966,
+      name: 'android',
+    },
+    {
+      has_synonyms: true,
+      is_moderator_only: false,
+      is_required: false,
+      count: 1124168,
+      name: 'html',
+    },
+    {
+      has_synonyms: true,
+      is_moderator_only: false,
+      is_required: false,
+      count: 1025504,
+      name: 'jquery',
+    },
+    {
+      has_synonyms: true,
+      is_moderator_only: false,
+      is_required: false,
+      count: 761792,
+      name: 'c++',
+    },
+    {
+      has_synonyms: true,
+      is_moderator_only: false,
+      is_required: false,
+      count: 755778,
+      name: 'css',
+    },
+  ],
+  has_more: true,
+  quota_max: 10000,
+  quota_remaining: 9684,
+};
+
+export default tags;
